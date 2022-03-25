3817198cfcd0adcb46a35c122d4d28f488221d08
Will Hurst <will.hurstjr@gmail.com>
2022-03-25 00:14:37 -0700

    final copy

--- app.js
+++ app.js
@@ -6,14 +6,83 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+
+let b = 4;
+
+let add = a + b;
+
+console.log(add);
+
+let minus = a - b;
+
+console.log(minus);
+
+let multiply = a * b;
+
+console.log(multiply);
+
+let divide = a / b;
+
+console.log(divide);
+
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let A = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+console.log(A + str)
+
+// answer is 1111
+
+console.log(A + str2)
+
+// answer is 11eleven
+
+console.log(A + isPresent)
+
+// answer is 12
+
+console.log(A + firstName)
+
+// answer is 11Jackie
+
+console.log(A + lastName)
+
+// answer is 11Chan
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log(d == str3);
+//true
+console.log(d === str3);
+//false
+console.log(!isPresent2);
+//true
+console.log("eleven" == str4 && d >= str3)
+//false
+console.log(!isPresent2 || isPresent2)
+//true
+console.log(0 == false)
+//true
+console.log(0 === false)
+//false
+console.log(0 != false)
+//false
+console.log(0 !== false)
+//true
